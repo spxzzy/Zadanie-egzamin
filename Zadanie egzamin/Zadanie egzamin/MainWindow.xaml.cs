1eadf02c42cabd5b99439649a6c071b4d8e55cbf
spxzzy <75693511+spxzzy@users.noreply.github.com>
2024-01-08 15:42:13 +0100

    Add files via upload

---
+++ Zadanie egzamin/Zadanie egzamin/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Arkusz1
+{
+   
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string postalCode = KodPocztowy.Text;
+            if (postalCode.Length == 5)
+            {
+                try
+                {
+                    int numericPostalCode = Convert.ToInt32(KodPocztowy.Text);
+                    MessageBox.Show("Dane prawidłowe");
+                }
+                catch (System.FormatException)
+                {
+                    MessageBox.Show("W kodzie pocztowym powinny byc tylko cyfry!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nieprawidłowa długość kodu pocztowego!");
+            }
+        }
+    }
+}
